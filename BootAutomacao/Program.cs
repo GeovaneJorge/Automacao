8873bc8aea6a8d3adaec64632166c701fe382342
Geovane Jorge <geovane.jorge@gmail.com>
2022-08-15 22:00:14 -0300

    Adicionado a primeira parte do projeto

---
+++ BootAutomacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using prmToolkit.Selenium;
+using prmToolkit.Selenium.Enum;
+
+namespace BootAutomacao
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string localDriver = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+			IWebDriver webDriver = WebDriverFactory.CreateWebDriver(Browser.Chrome,localDriver + "/driver");
+
+			webDriver.LoadPage(TimeSpan.FromSeconds(8), "https://www.portaltransparencia.gov.br/");
+			//webDriver.Navigate().GoToUrl("http://www.google.com/");
+
+		}
+	}
+}
